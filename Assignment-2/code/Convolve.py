feb4d1d7af2c529fbb35b9ac5bb91572ff488e37
Jarpula Bhanu Prasad <102509069+jarpula-Bhanu@users.noreply.github.com>
2022-09-19 22:04:14 +0530

    Add files via upload

---
+++ Assignment-2/code/Convolve.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+n = np.random.randint(-1000,1000)
+y = 0
+
+if n>=3:
+    for k in range(n+1,1000000):
+        y = y + 2**(n-k)
+else:
+    for k in range(4,1000000):
+        y = y + 2**(n-k)
+
+
+print("The value of n is : ",n)
+print("The value of output signal is : ",y)

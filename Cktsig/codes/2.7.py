8a71a147c0ac1e49620cb3c2ae4b6274da6abc41
Jarpula Bhanu Prasad <102509069+jarpula-Bhanu@users.noreply.github.com>
2022-10-10 08:46:59 +0530

    Add files via upload

---
+++ Cktsig/codes/2.7.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def U(t):
+    if (t < 0):
+        return 0
+    elif (t == 0):
+        return 0.5
+    else: 
+        return 1
+
+def v1(t):
+    if (t >= 0): 
+        return 4/3*(1 - np.exp(-t*1.5e6))*U(t)
+    else: 
+        return 0
+
+vc0 = np.vectorize(v1, otypes=['double'])
+
+t = np.linspace(0, 1e-5, 1000000)
+plt.plot(t, vc0(t))
+plt.xlabel('t (s)')
+plt.ylabel('$v_{C_0}(t)$ (V)')
+plt.grid()
+plt.savefig('./figs/2.7.png')
+plt.show()
